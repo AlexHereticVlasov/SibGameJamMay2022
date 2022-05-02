4ff7652d52234a634c71d7967ff9e97868d8d072
AlexHereticVlasov <62911435+AlexHereticVlasov@users.noreply.github.com>
2022-05-02 20:35:21 +0300

    Interactable Viev Fix

---
+++ Assets/Scripts/SpriteBean.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpriteBean", menuName = "ScriptableObject/SpriteBean")]
+public class SpriteBean : ScriptableObject
+{
+    [SerializeField] private Sprite[] _sprites;
+
+    public Sprite this[int index]=> _sprites[index];
+}
--- Assets/Scripts/Viev/MainMenu.cs
+++ Assets/Scripts/Viev/MainMenu.cs
@@ -13,37 +13,3 @@ public sealed class MainMenu : MonoBehaviour
 
     public void Quit() => Application.Quit();
 }
-
-
-public sealed class PauseMenu : MonoBehaviour
-{
-    [SerializeField] private GameObject _menuPanel;
-    [SerializeField] private SceneLoader _sceneLoader;
-
-    public void ChangeActivity()
-    {
-        if (Time.timeScale == 1)
-            Open();
-        else
-            Close();
-    }
-
-    public void GoToMain()
-    {
-        Close();
-        //ToDo: Change Main menu index here
-        _sceneLoader.LoadScene(0);
-    }
-
-    private void Close()
-    {
-        _menuPanel.SetActive(false);
-        Time.timeScale = 1;
-    }
-
-    private void Open()
-    {
-        _menuPanel.SetActive(true);
-        Time.timeScale = 0;
-    }
-}
