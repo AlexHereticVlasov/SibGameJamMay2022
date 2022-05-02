a7517ef6f26d903c62542e88f30d27ced8b6b230
AlexHereticVlasov <62911435+AlexHereticVlasov@users.noreply.github.com>
2022-05-02 21:31:58 +0300

    ConfigUpdate

--- Assets/Scripts/PlayerConfig.cs
+++ Assets/Scripts/PlayerConfig.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerConfig", menuName = "ScriptableObject/PlayerConfig")]
 public class PlayerConfig : ScriptableObject
 {
+    [Header("Landing Settings")]
     [SerializeField] private float _speedOnGround;
+    [Header("In Air Settings")]
     [SerializeField] private float _speedInAir;
+    [SerializeField] private float _risingForce;
     [SerializeField] private Vector2 _gravity;
 
     public float SpeedOnGround => _speedOnGround;
     public float SpeedInAir => _speedInAir;
+
+    public float RisingForce => _risingForce;
     public Vector2 Gravity => _gravity;
 }
